fab152623ac14501ed0bcc2d00ac749f14d6d056
Levy Araujo <levy4raujo@gmail.com>
2022-09-21 16:45:21 -0300

    Exercício 4.1 parte 1 concluído

---
+++ fundamentos/bloco4-4.1/4.1-parte1.js
@@ -0,0 +1,11 @@
+const myName = "Levy";
+const birthCity = "Aracaju";
+let birthYear = 1998;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "SP";
+console.log(birthCity);
+//Erro por tentar alterar uma constante
