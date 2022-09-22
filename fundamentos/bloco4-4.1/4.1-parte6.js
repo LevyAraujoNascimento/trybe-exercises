afb6f88df33325680b5b5ac57868dcc7855e70fe
Levy Araujo <levy4raujo@gmail.com>
2022-09-22 10:04:44 -0300

    Exercício 4.1 parte 6 concluído

---
+++ fundamentos/bloco4-4.1/4.1-parte6.js
@@ -0,0 +1,7 @@
+let seletivo = "lista";
+switch(seletivo){
+    case 'aprovada': console.log("Parabéns,você foi aprovada(o)"); break;
+    case 'lista': console.log("Você está na nossa lista de espera"); break;
+    case 'reprovada': console.log("Você foi reprovada(o)"); break;
+    default: console.log("informação incorreta");
+}
