e569f7556d33edc4eebe00e54a43b9f4b2be1771
Levy Araujo <levy4raujo@gmail.com>
2022-09-22 16:56:22 -0300

    Exercício 4.2 parte 1 concluído

---
+++ fundamentos/bloco4-4.2/4.2-parte1.js
@@ -0,0 +1,14 @@
+/! 1º Questão /
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu1[1];
+console.log(menuServices);
+
+/! 2º Questão /
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf("Portfólio");
+console.log(indexOfPortfolio);
+
+/! 3º Questão /
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push("Contato");
+console.log(menu3);
