2e5401e37ebf21b784fa13dd218839670f3eb273
Levy Araujo <levy4raujo@gmail.com>
2022-10-04 12:11:30 -0300

    Exercício 5.4 Final 3º requisito concluído

--- fundamentos/bloco5-5.4/5.4-Final.js
+++ fundamentos/bloco5-5.4/5.4-Final.js
@@ -1,11 +1,13 @@
 let inputFundo = document.getElementById('inputFundo');
 let inputTexto = document.getElementById('inputTexto');
+let inputTamanhoFonte = document.getElementById('inputTamanhoTexto');
 let titulo = document.getElementById('titulo');
 let corpo = document.getElementById('corpo');
 let loremIpsum = document.getElementById('loremIpsum');
 
 inputFundo.addEventListener('input',mudarFundo);
 inputTexto.addEventListener('input',mudarCorTexto);
+inputTamanhoFonte.addEventListener('input',mudarTamanhoFonte);
 
 function mudarFundo(evento){
     let texto = evento.target.value;
@@ -15,4 +17,13 @@ function mudarFundo(evento){
 function mudarCorTexto(evento){
     let texto = evento.target.value;
     loremIpsum.style.color = texto;
+}
+
+function mudarTamanhoFonte(evento){
+    let texto = evento.target.value;
+    if(texto === ""){
+        loremIpsum.style.fontSize = "16px";
+    }else{
+    loremIpsum.style.fontSize = texto + "px";
+    }
 }
