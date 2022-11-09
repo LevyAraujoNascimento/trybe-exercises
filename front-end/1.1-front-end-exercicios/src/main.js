d5a69ea7db8cafaf925ea1a95f6997529bf3a04d
Levy Araujo <levy4raujo@gmail.com>
2022-11-09 17:12:39 -0300

    Front End 1.1 final

--- front-end/1.1-front-end-exercicios/src/main.js
+++ front-end/1.1-front-end-exercicios/src/main.js
@@ -1,2 +1,9 @@
-import { nanoid } from "nanoid";
-console.log(nanoid());
+import { nanoid } from 'nanoid'
+import "./style.css";
+
+const botao = document.querySelector('button');
+const text = document.querySelector('h2');
+
+botao.addEventListener('click', () => {
+    text.innerText = nanoid();
+});
