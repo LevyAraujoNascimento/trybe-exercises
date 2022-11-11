3fe116cf4766990a4ff0fee9b5d5c2ad9e769297
Levy Araujo <levy4raujo@gmail.com>
2022-11-11 16:00:34 -0300

    Front End 1.4 Teste

---
+++ front-end/1.4-front-end-exercicios/src/main.js
@@ -0,0 +1,11 @@
+import "./style.css"
+import fetch from 'node-fetch';
+
+const cep = '30130-010'; 
+try {
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  const data = await response.json();
+  console.log(data);
+} catch(error) {
+  console.log(error)
+}
