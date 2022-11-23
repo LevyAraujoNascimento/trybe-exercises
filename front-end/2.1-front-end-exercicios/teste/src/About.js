6dce06db71c90582f1d7e6b9f28f6b90f0624ae5
Levy Araujo <levy4raujo@gmail.com>
2022-11-23 12:19:20 -0300

    Front End 2.1 parte 1 concluído

---
+++ front-end/2.1-front-end-exercicios/teste/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+        <>
+        <h1>Levy Araujo Nascimento</h1>
+        <p>Sou um estudadnte da Trybe, testando React</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>Criativo</li>
+            <li>Dedicado</li>
+            <li>Disposto a aprender</li>
+        </ul>
+        </>
+    );
+  }
+}
+
+export default About;
