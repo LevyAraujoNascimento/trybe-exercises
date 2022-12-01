4b274c272ca4e07cc58d544b80702517f9b3e09e
Levy Araujo <levy4raujo@gmail.com>
2022-12-01 10:14:10 -0300

    Exercício Front End 3.2 parte 2

--- front-end/3.1-front-end-exercicios/frontend3.2/my-form-1.0/src/App.js
+++ front-end/3.1-front-end-exercicios/frontend3.2/my-form-1.0/src/App.js
@@ -4,26 +4,32 @@ import './App.css';
 class App extends React.Component {
   
   state = {
-    valor: '',
+    Valor: '',
+    BoxUm: '',
+    BoxDois: '',
+    TextArea: '',
   }
   
-  click = (event) => {
+  click = ({ target }) => {
+    const { name, value } = target;
     this.setState({
-      valor: event.target.value,
+      [name]: value,
     });
   }
 
   render(){
+    const { Valor, BoxUm, BoxDois, TextArea } = this.state; 
+
     return (
       <>
-        <select>
-          <option>Valor 1</option>
-          <option>Valor 2</option>
-          <option>Valor 3</option>
+        <select name='Valor' value={ Valor } onChange={ this.click }>
+          <option value='Valor 1'>Valor 1</option>
+          <option value='Valor 2'>Valor 2</option>
+          <option value='Valor 3'>Valor 3</option>
         </select>
-        <input value={ this.state.valor } onChange={ this.click }></input>
-        <input></input>
-        <textarea></textarea>
+        <input name='BoxUm' value={ BoxUm } onChange={ this.click }></input>
+        <input name='BoxDois' value={ BoxDois } onChange={ this.click }></input>
+        <textarea name='TextArea' value={ TextArea } onChange={ this.click }></textarea>
       </>
     )
   }
